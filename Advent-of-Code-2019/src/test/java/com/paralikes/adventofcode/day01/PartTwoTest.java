3ac7183138acfdad8cb995faa2303f6c058642f1
paralikes <eparalikes@gmail.com>
2019-12-07 17:39:29 +0100

    Convert Day 01 to challenge structure

--- Advent-of-Code-2019/src/test/java/com/paralikes/adventofcode/day01/PartTwoTest.java
+++ Advent-of-Code-2019/src/test/java/com/paralikes/adventofcode/day01/PartTwoTest.java
@@ -1,24 +1,36 @@
 package com.paralikes.adventofcode.day01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import com.paralikes.adventofcode.ChallengeException;
+import com.paralikes.adventofcode.ChallengeTest;
+import com.paralikes.adventofcode.FileInput;
+import com.paralikes.adventofcode.IChallenge;
 
-class PartTwoTest {
+class PartTwoTest extends ChallengeTest<FileInput, Integer> {
 
 
-	@Test
-	void test() {
+	@Override
+	public IChallenge<FileInput, Integer> getChallenge() {
+		return new PartTwo();
+	}
+
+	@Override
+	public FileInput getInput() {
+		return new FileInput("./src/test/resources/Day01/input.txt");
+	}
+
+	@Override
+	public Integer getOutput() {
+		return 4896902;
+	}
+
+	
+	@Override
+	public void testRequirements() throws ChallengeException {
 		assertEquals(2, new PartTwo().calculateFuel(14));
 		assertEquals(966, new PartTwo().calculateFuel(1969));
 		assertEquals(50346, new PartTwo().calculateFuel(100756));
-		
-		assertEquals(4896902, new PartTwo().apply("./src/test/resources/Day01/input.txt"));
-	}
-	
-	@Test
-	void result() {
-		System.out.println(new PartTwo().apply("./src/test/resources/Day01/input.txt"));
 	}
 
 }
